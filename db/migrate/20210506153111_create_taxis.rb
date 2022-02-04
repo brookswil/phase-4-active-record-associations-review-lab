757a4d5a7de8407eebbc6bbb2a19e060caf4a92f
Brooks_Wilcox <brookswill88@gmail.com>
2022-02-04 09:32:21 -0700

    commit tree dc2fcdfa25db918c38e9b38520f41aa78c0134f7

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,7 +1,7 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
-
+      t.integer :passenger_id, null: false
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# passenger1 = Passenger.create(name: "Brooks")
+# passenger2 = Passenger.create(name: "Myckala")
+
+# taxi1 = Taxi.create(name: "dopender")
+# taxi2 = Taxi.create(name: "Jason")
+
+# Ride.create(passenger_id: passenger1.id, taxi_id: taxi1.id)
+# Ride.create(passenger_id: passenger2.id, taxi_id: taxi2.id)
